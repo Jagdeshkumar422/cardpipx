508ca32242cb979eccc4412b4144f55ca977da7c
Jagdeshkumar422 <118194807+Jagdeshkumar422@users.noreply.github.com>
2025-02-03 14:29:36 +0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-// const backend_URL = "https://cardpip-x-backend.vercel.app/api";
-const backend_URL = "http://localhost:3000/api";
+const backend_URL = "https://cardpip-x-backend.vercel.app/api";
+// const backend_URL = "http://localhost:3000/api";
 export default backend_URL;
